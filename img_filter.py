288f9acd9b871576fe4b5c7f87b3c327f50f4d45
dmartin305 <dmartin305@gatech.edu>
2022-11-05 13:41:54 -0400

    add seperate image manipulation script

---
+++ img_filter.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+
+def apply_guassian_filter(img):
+    # Image is passed in as an cv::Mat object
+    # https://docs.opencv.org/4.x/d3/d63/classcv_1_1Mat.html
+    # 
+    # Apply the filter here and return the image post-filter
+
+    return img
